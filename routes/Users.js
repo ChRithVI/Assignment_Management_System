a2373847b923640904f86f8d40196577b9500332
Charitha Bandara <charithac.cb@gmail.com>
2024-05-06 16:42:30 +0530

    Still routes
    User login parts

--- routes/Users.js
+++ routes/Users.js
@@ -1,3 +1,5 @@
+const { hash } = require("bcrypt");
+
 const express = require(id: 'express')
 
 const users = express.Router();
@@ -29,9 +31,50 @@ users.post('/register', (req,res) => {
     })
         .then( resolve: user => {
             if(!user){
-                
+                bcrypt.hash(req.body.password, salt: 10, cb:(err, hash) => {
+                    userData.password = hash
+                    User.create(userData)
+                        .then(user = > {
+                            res.json({status: user.email + "registered"})
+                        })
+                        ,.catch( onrejected: err => {
+                            res.send("error" + err);
+                        })
+                })
+            } else{
+                res.json({error:"User already registered"})
             }
         })
+        .catch( onrejected: err => {
+            res.send("error" +err);
+        })
+})
 
-
+users.post('/login', (req,res) => {
+    User.findOne({
+        email: req.body.email
+    })
+        .then( resolve: user => {
+            if(user){
+                if(bcrypt.compareSync(req.body.password, user.password)){
+                    const payload={
+                        _id: user._id,
+                        first_name: user.first_name,
+                        last_name: user.last_name,
+                        email: user.email
+                    }
+                    let token = jwt.sign(payload, process.env.SECRET_KEY, {
+                        expiresIn: 1440
+                    })
+                    res.send(token)
+                }else {
+                    res.json({error: "User does not exist in the system"})
+                }
+            } else{
+                res.json({error: "User does not exist in the system"})
+            }
+        })
+        .catch(onrejected err => {
+            res.send("error" + err);
+        })
 })
